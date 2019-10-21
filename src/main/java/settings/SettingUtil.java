ce22356490468641cc2986b556a0ec312cd85f3a
Albina <al.phant@yandex.ru>
2019-10-21 18:22:09 +0400

    Repainting fix

--- src/main/java/settings/SettingUtil.java
+++ src/main/java/settings/SettingUtil.java
@@ -6,8 +6,8 @@ public class SettingUtil {
     public static int SCALE = 20;
     public static int SNAKE_LENGTH = 3;
     public static int FROG_COUNT = 3;
-    public static long FROG_SPEED = 1200;
-    public static long SNAKE_SPEED = 200;
+    public static long FROG_SPEED = 1000;
+    public static long SNAKE_SPEED = 100;
     public static int MAX_X = WIDTH / SCALE;
     public static int MAX_Y = (HEIGHT - 30) / SCALE;
 }
--- src/main/java/view/GamePanel.java
+++ src/main/java/view/GamePanel.java
@@ -12,20 +12,12 @@ import java.util.List;
 
 public class GamePanel extends JPanel implements DrawObserver {
     private final int width, height, scale;
-//    private volatile Set<Frog> frogs = Collections.synchronizedSet(new HashSet<>());
-//    private volatile List<Point> snakeBody = Collections.synchronizedList(new LinkedList<>());
-//    private Set<Point> activePoints = new HashSet<>();
-    private Graphics2D g2d;
-//    private int[] changes;
-    private volatile Point[][] field;
 
-    private boolean snakeRepaint = false;
+    private Graphics2D g2d;
+    private Point[][] field;
 
     private GameStatus statusPanel;
 
-    private final Object lockObject = new Object();
-
-//    private Status
     public GamePanel(int width, int height, int scale) {
         this.width = width;
         this.height = height;
@@ -58,8 +50,6 @@ public class GamePanel extends JPanel implements DrawObserver {
         Arrays.stream(field).flatMap(Arrays::stream).forEach(point -> {
             if (null != point) paintAct(point);
         });
-//        snakeBody.forEach(this::paintAct); // todo ConcurrentModificationException
-//        frogs.forEach(frog -> paintAct(frog.getPosition()));
     }
 
     public void paintDots() {
@@ -91,102 +81,24 @@ public class GamePanel extends JPanel implements DrawObserver {
     @Override
     public void update(Point[][] grid, int minx, int maxx, int miny, int maxy) {
         synchronized (field) {
-//            changes = new int[]{1,2};
-//            changes = grid;
-//            Point startPoint = null;
-snakeRepaint = true;
-//            Point[][] changes = new Point[maxy + 1][maxx + 1];
             for (int i = miny, j = minx; i <= maxy; i++, j++) {
                 field[i] = Arrays.copyOfRange(grid[i], minx, maxx + 1);
-//            System.out.println("grid i " + Arrays.deepToString(grid[i]));
-//            System.out.println("copy grid i " + Arrays.deepToString(Arrays.copyOfRange(grid[i], minx, maxx + 1)));
             }
-//
-//            for (int i = 0; i < grid[0].length; i++) {
-//                for (int j = 0; j < grid.length; j++) {
-//                    if (null != grid[j][i]) {
-//                        startPoint = grid[j][i];
-//                        break;
-//                    }
-//                }
-//                if (null != startPoint) break;
-//            }
 
             Rectangle place = new Rectangle(minx * scale, miny * scale, (maxx - minx) * scale + scale , (maxy - miny) * scale + scale);
-//            if (character instanceof Snake) {
-////                snakeRepaint=true;
-//                Snake snake = (Snake) character;
-//                snakeBody = snake.getBody(); //todo update only snake
-//    //            activePoints.addAll(snake.getBody());
-//                statusPanel.setScore(snake.getXp());
-//                place = getRedrawClip((Snake) character);
-//            } else if (character instanceof Frog) {
-////                snakeRepaint=false;
-//                if (character.isAlive()) {
-//    //                activePoints.
-//                    frogs.add((Frog) character); //todo update only frogs
-//                } else {
-//                    frogs.remove(character); // todo npex
-//                }
-//                place = getRedrawClip((Frog) character);
-//            }
-////                    paintImmediately(place);
-////        }
-//        repaint(place);
-                    paintImmediately(place);
+            paintImmediately(place);
 //            repaint(place);
         }
     }
 
     @Override
     public void clear() {
-//        frogs = new HashSet<>();
-//        snakeBody = new LinkedList<>();
+        field = new Point[height][width];
         repaint();
     }
 
     public void setStatusPanel(GameStatus statusPanel) {
         this.statusPanel = statusPanel;
     }
-
-    private Rectangle getRedrawClip(Snake snake) {
-        int minx = width, miny = height, maxx = 0, maxy = 0;
-
-        for (Point point: snake.getBody()) {
-//            System.out.println("Snake point " + point.getX() + " " + point.getY());
-            if (point.getX() > maxx) maxx = point.getX();
-            if (point.getX() < minx) minx = point.getX();
-            if (point.getY() > maxy) maxy = point.getY();
-            if (point.getY() < miny) miny = point.getY();
-        }
-        if (minx - scale < 0 || maxx + scale*2 > width) {
-            minx = 0;
-            maxx = width;
-        }
-        if (miny - scale < 0 || maxy + scale*2 > height) {
-            miny = 0;
-            maxy = height;
-        }
-//        System.out.println("snake rect x " + (minx - scale) + " y " + (miny - scale) + " w " + (maxx - minx + scale  * 3) + " h " +(maxy - miny + scale  * 3));
-        return new Rectangle(minx - scale, miny - scale, maxx - minx + scale * 3, maxy - miny + scale * 3);
-    }
-
-    private Rectangle getRedrawClip(Frog frog) {
-        Point point = frog.getPosition();
-        int x = 0, y = 0, clipWidth = width, clipHeight = height;
-
-        if (point.getX() - scale < 0 || point.getX() + scale*2 >= width) {
-            x = point.getX() - scale;
-            clipWidth = scale * 3;
-        }
-
-        if (point.getY() - scale < 0 || point.getY() + scale*2 >= height) {
-            y = point.getY() - scale;
-            clipHeight = scale * 3;
-        }
-
-        return new Rectangle(x, y, clipWidth, clipHeight);
-    }
-
 
 }
